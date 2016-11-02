7ff73b6a27eefa7ca71edbdbd9c80cbd694cb6f0
huangxiaoyan <842879333@qq.com>
2016-11-02 14:13:31 +0800

    初次提交

---
+++ app/src/main/java/cn/zx/map1102/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.zx.map1102;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String str = "hhh";
+
+    }
+}
